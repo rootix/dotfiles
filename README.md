17306cd8b89596549d8457e94f505c78af309e6f
Pascal Mathys <pascal.mathys@rootix.ch>
2019-09-21 14:21:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# dotfiles
+
+These are my settings files for my development environment. They are updated periodically and only serve as a backup. There is no symlinking involved :-)
+
+## Updating configs
+Run `.\backup.ps1` in console to update configs and publish them to the origin repository.
---
+++ backup.ps1
@@ -0,0 +1,14 @@
+# VSCode
+Copy-Item "C:\Users\Pascal\AppData\Roaming\Code\User\settings.json" -Destination ".\vscode\"
+Copy-Item "C:\Users\Pascal\AppData\Roaming\Code\User\keybindings.json" -Destination ".\vscode\"
+
+# Git
+Copy-Item "C:\Users\Pascal\.gitconfig" -Destination ".\git\"
+
+# Windows Terminal
+Copy-Item "C:\Users\Pascal\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json" -Destination ".\windows-terminal\"
+
+# Publish to origin
+git add -A
+git commit -m "Update dotfiles"
+git push
